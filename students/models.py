c1bf0af029253c320539abce2fe9992dbd11a61c
leoseis <eboscrib2@yahoo.com>
2024-01-22 11:57:08 +0100

    tables added but got stuck with a typpo error lenght instead of length

--- students/models.py
+++ students/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+
+from django.db import models
+
+class Student(models.Model):
+    studentId = models.AutoField(primary_key=True)
+    FirstName = models.CharField(max_length=100)
+    LastName = models.CharField(max_length=100)
+    RegistrationNo = models.CharField(max_length=100)
+    Email = models.CharField(max_length=100)
+    Course = models.CharField(max_length=100)
+
+    
+
+    
+
+    def __str__(self):
+        return 
+
+    def __unicode__(self):
+        return 
