fe622568fc9433217e815d2eaf549512e8817c17
leoseis <eboscrib2@yahoo.com>
2024-01-23 21:29:36 +0100

    fixxed import typo error then rendered api

---
+++ students/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from .views import StudentView
+
+urlpatterns = [
+    path('students/', StudentView.as_view())
+]
--- students/views.py
+++ students/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from .serializers import StudentSerializer
+from django.http.response import JsonResponse
 
-# Create your views here.
+
+class StudentView(APIView):
+
+    def post (self, request):
+        data = request.data 
+        serializer = StudentSerializer(data=data)
+        
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse('Student Created Succesfuly', safe=False)
+        
+        return JsonResponse('Failed to add Student', safe=False)
