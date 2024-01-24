89071e533488cafe43c22eff95fa4e9c9f477011
leoseis <eboscrib2@yahoo.com>
2024-01-24 16:08:20 +0100

    get method addded to my api

--- students/views.py
+++ students/views.py
@@ -1,6 +1,9 @@
 from rest_framework.views import APIView
 from .serializers import StudentSerializer
 from django.http.response import JsonResponse
+from .models import Student
+from django.http.response import Http404
+from rest_framework.response import Response
 
 
 class StudentView(APIView):
@@ -14,3 +17,21 @@ class StudentView(APIView):
             return JsonResponse('Student Created Succesfuly', safe=False)
         
         return JsonResponse('Failed to add Student', safe=False)
+    
+
+    def get_student(self, pk):
+        try:
+            student = Student.objects.get(studentId=pk)
+            return student
+        except Student.DoesNotExist():
+            raise Http404
+        
+    def get(self, request, pk=None):
+        if pk:
+            data =self.get_student(pk)
+            serializer =StudentSerializer(data)
+        else:
+            data = Student.objects.all()
+            serializer = StudentSerializer(data, many=True)
+
+        return Response(serializer.data)
