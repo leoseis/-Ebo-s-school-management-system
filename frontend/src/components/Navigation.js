d988f4f3e25a6b6904ba35f0bbd3ca15cdc3470e
leoseis <eboscrib2@yahoo.com>
2024-02-05 09:06:57 +0100

    nav bar created from bootsrap and imported aand implemented in Navigation.js file

--- frontend/src/components/Navigation.js
+++ frontend/src/components/Navigation.js
@@ -1,8 +1,23 @@
 import React from "react";
+import{ Navbar} from 'react-bootstrap';
+import logo from '../static/logo.png';
 
 const Navigation = ()=> {
     return (
-        <h1>The Navigation Component </h1>
+        <div>
+            <Navbar bg="dark" variant="dark">
+        <Navbar.Brand  href="None">
+            <img
+            alt=""
+              src={logo}
+              width="40"
+              height="50"
+              className="d-inline-block align-center"
+            />{' '}
+             Student managent Application
+        </Navbar.Brand>
+    </Navbar>
+        </div>
     );
 };
 
