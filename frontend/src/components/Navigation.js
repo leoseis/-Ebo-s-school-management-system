9d58778545edf81fb76702d506b268798bcb905d
leoseis <eboscrib2@yahoo.com>
2024-02-05 20:51:28 +0100

    removed a bug when in wanted to apply padding to the nav bar

--- frontend/src/components/Navigation.js
+++ frontend/src/components/Navigation.js
@@ -1,24 +1,25 @@
-import React from "react";
-import{ Navbar} from 'react-bootstrap';
-import logo from '../static/logo.png';
+    import React from "react";
+    import{ Navbar} from 'react-bootstrap';
+    import logo from '../static/logo.png';
+    import "../App.css";
 
-const Navigation = ()=> {
-    return (
-        <div>
-            <Navbar bg="dark" variant="dark">
-        <Navbar.Brand  href="None">
-            <img
-            alt=""
-              src={logo}
-              width="40"
-              height="50"
-              className="d-inline-block align-center"
-            />{' '}
-             Student managent Application
-        </Navbar.Brand>
-    </Navbar>
-        </div>
-    );
-};
+    const Navigation = ()=> {
+        return (
+            <div>
+        <Navbar bg="dark" variant="dark"expand ='lg'>
+            <Navbar.Brand className="app-logo" href="/">
+                <img
+                alt=""
+                src={logo}
+                width="40"
+                height="50"
+                className="d-inline-block align-center"
+                />{' '}
+               All Joy Student managent Application Portal
+            </Navbar.Brand>
+        </Navbar>
+            </div>
+        );
+    };
 
-export default Navigation;
+    export default Navigation;
