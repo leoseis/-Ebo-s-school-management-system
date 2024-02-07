bca5855334015e76bc11782c3f9bbe098d185cd4
leoseis <eboscrib2@yahoo.com>
2024-02-07 03:20:42 +0100

    added side bar to the navigation bar

--- frontend/src/components/Navigation.js
+++ frontend/src/components/Navigation.js
@@ -3,6 +3,17 @@
     import logo from '../static/logo.png';
     import "../App.css";
 
+    import {
+        CDBSidebar,
+        CDBSidebarHeader,
+        CDBSidebarMenuItem,
+        CDBSidebarContent,
+        CDBSidebarMenu,
+        CDBSidebarSubMenu,
+        CDBSidebarFooter,
+      } from 'cdbreact';
+
+
     const Navigation = ()=> {
         return (
             <div>
@@ -18,6 +29,31 @@
                All Joy Student managent Application Portal
             </Navbar.Brand>
         </Navbar>
+            
+            <div>
+                
+            <CDBSidebar>
+        <CDBSidebarHeader prefix={<i className="fa fa-bars" />}>Contrast</CDBSidebarHeader>
+        <CDBSidebarContent>
+          <CDBSidebarMenu>
+            <CDBSidebarMenuItem icon="th-large">Dashboard</CDBSidebarMenuItem>
+            <CDBSidebarMenuItem icon="sticky-note">Components</CDBSidebarMenuItem>
+            <CDBSidebarMenuItem icon="credit-card" iconType="solid">
+              Metrics
+            </CDBSidebarMenuItem>
+          </CDBSidebarMenu>
+        </CDBSidebarContent>
+
+        <CDBSidebarFooter style={{ textAlign: 'center' }}>
+          <div
+            className="sidebar-btn-wrapper"
+            style={{padding: '20px 5px'}}
+          >
+            Sidebar Footer
+          </div>
+        </CDBSidebarFooter>
+      </CDBSidebar>
+            </div>
             </div>
         );
     };
