0ca125199c546795c81afe50489fdf6cac052907
leoseis <eboscrib2@yahoo.com>
2024-02-08 16:52:19 +0100

    added a carousel slide with my statics pictures

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navigation from './components/Navigation';
+import Home from './components/Home';
+import { BrowserRouter,Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
+    <BrowserRouter>
     <Navigation />
+    <Routes>
+    <Route exact path="/" element={<Home/>} />
+    </Routes>
+    </BrowserRouter>
   );
 }
 
