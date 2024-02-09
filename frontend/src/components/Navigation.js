098c98b90b74196ac502f5113a51339b7161960d
leoseis <eboscrib2@yahoo.com>
2024-02-09 13:56:12 +0100

    created links for home, students, management list for display

--- frontend/src/components/Navigation.js
+++ frontend/src/components/Navigation.js
@@ -1,50 +1,54 @@
-    import React from "react";
-    import{ Navbar} from 'react-bootstrap';
-    import logo from '../static/logo.png';
-    import "../App.css";
+import React from 'react';
+import {
+  CDBSidebar,
+  CDBSidebarContent,
+  CDBSidebarHeader,
+  CDBSidebarMenu,
+  CDBSidebarMenuItem,
+} from 'cdbreact';
+import {NavLink} from 'react-router-dom';
+import {Navbar} from 'react-bootstrap';
+import logo from '../static/logo.png';
+import "../App.css";
 
-    import {
-        CDBSidebar,
-        CDBSidebarHeader,
-        CDBSidebarMenuItem,
-        CDBSidebarContent,
-        CDBSidebarMenu,
-      } from 'cdbreact';
 
-
-    const Navigation = ()=> {
-        return (
-            <div>
-        <Navbar bg="dark" variant="dark"expand ='lg'>
-            <Navbar.Brand className="app-logo" href="/">
-                <img
-                alt=""
-                src={logo}
-                width="40"
-                height="50"
-                className="d-inline-block align-center"
-                />{' '}
-               All Joy Student managent Application Portal
-            </Navbar.Brand>
-        </Navbar>
-            
-      <div className="sidebar">
-            <CDBSidebar textColor="333"backgroundColor="fofofo">    
-        <CDBSidebarHeader prefix={<i className="fa fa-bars" />}>Navigation</CDBSidebarHeader>
+const Navigation = () => {
+  return (
+    <div>
+    <Navbar bg="dark" variant="dark" expand="lg" id="my-nav">
+        <Navbar.Brand className="app-logo" href="/">
+            <img
+              src={logo}
+              width="40"
+              height="50"
+              className="d-inline-block align-center"
+              alt="React Bootstrap logo"
+            />{' '}
+            Student Management System
+        </Navbar.Brand>
+    </Navbar>
+    <div className='sidebar'>
+    <CDBSidebar textColor="#333" backgroundColor="#f0f0f0">
+        <CDBSidebarHeader prefix={<i className="fa fa-bars" />}>
+          Navigation
+        </CDBSidebarHeader>
         <CDBSidebarContent>
           <CDBSidebarMenu>
-            <CDBSidebarMenuItem icon="home">Home</CDBSidebarMenuItem>
-            <CDBSidebarMenuItem icon="list">Students</CDBSidebarMenuItem>
-            <CDBSidebarMenuItem icon="user" iconType="solid">
-              Manage Students
-            </CDBSidebarMenuItem>
+            <NavLink exact to="/" activeClassName="activeClicked">
+              <CDBSidebarMenuItem icon="home">Home</CDBSidebarMenuItem>
+            </NavLink>
+            <NavLink exact to="/students" activeClassName="activeClicked">
+              <CDBSidebarMenuItem icon="list">Students List</CDBSidebarMenuItem>
+            </NavLink>
+            <NavLink exact to="/manage" activeClassName="activeClicked">
+              <CDBSidebarMenuItem icon="user">Manage Students</CDBSidebarMenuItem>
+            </NavLink>
           </CDBSidebarMenu>
         </CDBSidebarContent>
-
-       </CDBSidebar>
-      </div>
+      </CDBSidebar>
     </div>
-        );
-    };
+    </div>
+  );
+};
 
-    export default Navigation;
+export default Navigation;
