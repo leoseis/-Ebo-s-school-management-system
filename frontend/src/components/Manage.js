f39b60ae470dee92b6b9dc2223e290cb762bd9aa
leoseis <eboscrib2@yahoo.com>
2024-02-25 13:22:34 +0100

    defined add modal show

--- frontend/src/components/Manage.js
+++ frontend/src/components/Manage.js
@@ -1,72 +1,93 @@
-import React, { useEffect, useState } from 'react';
-import { Table } from 'react-bootstrap';
-import { Button, ButtonToolbar } from 'react-bootstrap';
-import { getStudents, deleteStudents} from '../services/StudentService';
-import AddStudentModal from './AddStudentModal';
-
+import React, { useEffect, useState } from "react";
+import { Table } from "react-bootstrap";
+import { Button, ButtonToolbar } from "react-bootstrap";
+import { getStudents, } from "../services/StudentService";
+import AddStudentModal from "./AddStudentModal";
 
 const Manage = () => {
   const [students, setStudents] = useState([]);
-  const [addModalshow, setAddModalshow] = useState(false);       // This ana add function this shows a new form when there is am add button
+  const [addModalshow, setAddModalshow] = useState(false); // This ana add function this shows a new form when there is am add button
+  const [isUpdated, setIsUpdated] = useState(false);
 
   useEffect(() => {
-   let mounted = true;
-   getStudents()
-     .then(data => {
-       if(mounted) {
-         setStudents(data)
-       }
-     })
-   return () => mounted = false;
- }, []);
+    let mounted = true;
+    if (students.length && !isUpdated) {
+      return;
+    }
+    getStudents().then((data) => {
+      if (mounted) {
+        setStudents(data);
+      }
+    });
+    return () => {
+      mounted = false;
+      setIsUpdated(false);
+    };
+  }, [isUpdated, students]);
 
-   const  handleAdd = (e) =>{                              // handleAdd handler
+  const handleAdd = (e) => {
+    // handleAdd handler
     e.preventDefault();
     setAddModalshow(true);
-   };
+  };
 
-   let AddModalClose = () => AddModalClose(false)   // whenever my form is closed set  to false
+  let AddModalClose = () => AddModalClose(false); // whenever my form is closed set  to false
 
-  return(
-   <div className="container-fluid side-container">
-   <div className="row side-row" >
-    <p id="before-table"></p>
-        <Table striped bordered hover className="react-bootstrap-table" id="dataTable">
-        <thead>
+  return (
+    <div className="container-fluid side-container">
+      <div className="row side-row">
+        <p id="before-table"></p>
+        <Table
+          striped
+          bordered
+          hover
+          className="react-bootstrap-table"
+          id="dataTable"
+        >
+          <thead>
             <tr>
-            <th>ID</th>
-            <th>First Name</th>
-            <th>Last Name</th>
-            <th>Registration No</th>
-            <th>Email</th>
-            <th>Course</th>
-            <th>Action</th>                  
+              <th>ID</th>
+              <th>First Name</th>
+              <th>Last Name</th>
+              <th>Registration No</th>
+              <th>Email</th>
+              <th>Course</th>
+              <th>Action</th>
             </tr>
-        </thead>
-        <tbody>
-            {students.map((stu) =>
-            <tr key={stu.id}>
+          </thead>
+          <tbody>
+            {students.map((stu) => (
+              <tr key={stu.id}>
                 <td>{stu.studentId}</td>
                 <td>{stu.FirstName}</td>
                 <td>{stu.LastName}</td>
                 <td>{stu.RegistrationNo}</td>
                 <td>{stu.Email}</td>
                 <td>{stu.Course}</td>
-                <td> 
-                  <Button className='mr-2' variant="primary">Update</Button>{' '}  
-                  <Button className='mr-2' variant="danger">Delete</Button>{' '}  
-
-                  </td>
-            </tr>)}
-        </tbody>
-    </Table>
-    <ButtonToolbar>  
-    <Button  variant="success"onClick={handleAdd}>Add Student </Button>{' '}
-    <AddStudentModal show={addModalshow}onHide={AddModalClose}>
-      </AddStudentModal>  
-    </ButtonToolbar>
+                <td>
+                  <Button className="mr-2" variant="primary">
+                    Update
+                  </Button>{" "}
+                  <Button className="mr-2" variant="danger">
+                    Delete
+                  </Button>{" "}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+        <ButtonToolbar>
+          <Button variant="primary" onClick={handleAdd}>
+            Add Student
+          </Button>
+          <AddStudentModal
+            show={addModalshow}
+            setUpdated={setIsUpdated}
+            onHide={AddModalClose}
+          ></AddStudentModal>
+        </ButtonToolbar>
+      </div>
     </div>
-  </div>
   );
 };
 
