ed5d62e760eaa2c65576a973dbcf474106b97d0c
leoseis <eboscrib2@yahoo.com>
2024-02-27 17:50:27 +0100

    fixed bug after 3 days  added an onClick event in the update button with imports created updatedStudentmodlcomponent

--- frontend/src/components/Manage.js
+++ frontend/src/components/Manage.js
@@ -3,11 +3,15 @@ import { Table } from "react-bootstrap";
 import { Button, ButtonToolbar } from "react-bootstrap";
 import { getStudents, } from "../services/StudentService";
 import AddStudentModal from "./AddStudentModal";
+import UpdateStudentModal from "./UpdateStudentModal";
 
 const Manage = () => {
   const [students, setStudents] = useState([]);
   const [addModalshow, setAddModalshow] = useState(false); // This ana add function this shows a new form when there is am add button
+  const [editModalShow, setEditModalshow] = useState(false);
+  const [editStudent, setEditStudent] = useState([]);
   const [isUpdated, setIsUpdated] = useState(false);
+
 
   useEffect(() => {
     let mounted = true;
@@ -31,8 +35,16 @@ const Manage = () => {
     setAddModalshow(true);
   };
 
-  let AddModalClose = () => AddModalClose(false); // whenever my form is closed set  to false
+  const handleUpdate = (e, stu) => {
+    // handleupdate handler
+    e.preventDefault();
+    setEditModalshow(true);
+    setEditStudent(stu);
+  };
 
+
+  let AddModalClose = () => AddModalClose(false);         // whenever my form is closed set  to false
+  let EditModelClose =()=> setEditModalshow(false);
   return (
     <div className="container-fluid side-container">
       <div className="row side-row">
@@ -65,9 +77,13 @@ const Manage = () => {
                 <td>{stu.Email}</td>
                 <td>{stu.Course}</td>
                 <td>
-                  <Button className="mr-2" variant="primary">
+                  <Button className="mr-2" variant="primary"
+                  onClick={event => handleUpdate(event, stu)}>
                     Update
                   </Button>{" "}
+                  <UpdateStudentModal show={editModalShow} student={editStudent} setUpdated={setIsUpdated}
+                    onHide={EditModelClose}>
+                    </UpdateStudentModal>
                   <Button className="mr-2" variant="danger">
                     Delete
                   </Button>{" "}
@@ -77,7 +93,7 @@ const Manage = () => {
           </tbody>
         </Table>
         <ButtonToolbar>
-          <Button variant="primary" onClick={handleAdd}>
+          <Button variant="primary" onClick={handleAdd}>        {/*handleAdd hanlder for event and it would be applied to the update*/}
             Add Student
           </Button>
           <AddStudentModal
--- frontend/src/services/StudentService.js
+++ frontend/src/services/StudentService.js
@@ -16,3 +16,14 @@ export function addStudent(student){                                          //
   })
     .then(response=>response.data) 
 }
+
+export function updateStudent(stuid, student) {
+  return axios.put('http://127.0.0.1:8000/students/' + stuid + '/', {
+    FirstName:student.FirstName.value,
+    LastName:student.LastName.value,
+    RegistrationNo:student.RegistrationNo.value,
+    Email:student.Email.value,
+    Course:student.Course.value
+  })
+   .then(response => response.data)
+}
